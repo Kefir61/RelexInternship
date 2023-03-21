a10cafa6cb125ad5c548108891774ac9e9412988
Дмитрий Лиманский <dmitry_limansky@relex.ru>
2023-03-21 17:55:47 +0300

    Добавил базовую настройку и структуру приложения

---
+++ webpack.config.js
@@ -0,0 +1,70 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        app: path.join(__dirname, 'src', 'index.tsx')
+    },
+    resolve: {
+        alias: {
+            '@components': path.resolve(__dirname, 'src/components'),
+            '@pages': path.resolve(__dirname, 'src/pages'),
+            '@utils': path.resolve(__dirname, 'src/utils'),
+            '@store': path.resolve(__dirname, 'src/store'),
+            '@assets': path.resolve(__dirname, 'src/assets'),
+        },
+        extensions: ['.ts', '.tsx', '.js', '.css', '.scss']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.s(a|c)ss$/,
+                use: [
+                    'style-loader',
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            modules: {
+                                localIdentName: '[local]',
+                            },
+                        },
+                    },
+                    'sass-loader',
+                ]
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                    },
+                ]
+            },
+        ]
+    },
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    devServer: {
+        port: 9000,
+        open: true,
+        compress: true,
+        hot: true,
+    },    
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, 'src', 'index.html')
+        })
+    ]
+};

b6e5b3056f66c361004d50c2639e26eb3dda047e
Egor <yegor.yermakov.2001@mail.ru>
2023-03-30 17:33:02 +0300

    #100 preparing a login for the demo

--- src/utils/api/apiService.ts
+++ src/utils/api/apiService.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 axios.interceptors.request.use((config) => {
-  config.headers.Authorization = `Bearer ${localStorage.getItem("token")}`;
+  config.headers.Authorization = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIyIiwibmFtZSI6ImpvaG5kb2UiLCJpYXQiOjk5OTk5OTk5OSwiYXV0aG9yaXRpZXMiOlsiZW1wbG95ZWUiLCJzeXN0ZW1fYWRtaW5pc3RyYXRvciIsInN0b3JlX2FkbWluaXN0cmF0b3IiLCJldmVudF9hZG1pbmlzdHJhdG9yIiwiYWNjcnVhbF9hZG1pbmlzdHJhdG9yIiwiYnVkZ2V0X293bmVyIiwiYXVkaXRvciIsImV2ZW50X29yZ2FuaXplciJdfQ.sqjyo3YQSc-kGLoyyDRIYiHeDQu8nWJyhoMxMnMDC14`;
   return config;
 });

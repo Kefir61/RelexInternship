5d42cc4420014cde374f8c66b2973fdf2fc9af12
Сидоров Олег <oleg_sidorov@relex.ru>
2023-04-10 15:39:20 +0300

    #147 start layout for Favorite page

--- src/utils/funcs/helpers/generateFio.ts
+++ src/utils/funcs/helpers/generateFio.ts
@@ -1,4 +1,4 @@
 import { IUser } from "src/utils/types";
 
-export const generateFio = (user:IUser) => `${user.lastName} ${user.firstName[0]}. ${user.patronymic[0]}`
+export const generateFio = (user:IUser) => `${user.lastName} ${user.firstName[0]}. ${user.patronymic?user.patronymic[0]:''}`
     
